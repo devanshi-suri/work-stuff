7c31b01ab74293d0603b7d0a33a88583c119f96d
devanshi-suri <suridevansh@gmail.com>
2025-07-18 18:01:30 +0530

    playing with smolagents(HF)

---
+++ sample1.py
@@ -0,0 +1,24 @@
+from smolagents import CodeAgent, InferenceClientModel
+
+# Initialize a model (using Hugging Face Inference API)
+model = InferenceClientModel()  # Uses a default model
+
+# Create an agent with no tools
+agent = CodeAgent(tools=[], model=model)
+
+# Run the agent with a task
+result = agent.run(
+    "What is the code to create a binary search tree?")
+print(result)
+
+from smolagents import DuckDuckGoSearchTool
+
+
+agent = CodeAgent(
+    tools=[DuckDuckGoSearchTool()],
+    model=model,
+)
+
+# Now the agent can search the web!
+result = agent.run("What is the current weather in Paris?")
+print(result)
